dbc754211135d64397c439c648a1b42e0736a661
Apoorva <Apoorva>
2019-06-04 02:15:57 +0530

    more functions

--- myshell.c
+++ myshell.c
@@ -104,6 +104,146 @@ void execComm(char ** commands)
 	}
 }
 
+//To execute pipeline commands we need to change the file descriptor out the ends to that of stdout and stdin
+void execPipeCommands(char **commands, char **pipecommands)
+{
+	int pipe_file_desc[2];
+
+	if(pipe(pipe_file_desc)<0)
+	{
+		printf("\n%sError in initialising the pipe!",KRED );
+		return ;
+	}
+
+	pid_t p1=fork();
+
+	if(p1<0)
+		printf("\n%sError in forking write child process",KRED );
+
+	
+	if(p1 == 0)
+	{
+		//Close the read end
+		close(pipe_file_desc[0]);
+
+		//Make stdout file descriptor point to the write pipe end
+		dup2(pipe_file_desc[1],STDOUT_FILENO);
+
+		//Unlink pipe write end
+		close(pipe_file_desc[1]);
+
+		if(execvp(commands[0],commands)<0)
+		{
+			printf("\n%sError in executing command 1!",KRED );
+			exit(0);	
+		}
+	}
+	else
+	{
+		pid_t p2 = fork();
+		if(p2 < 0)
+		{
+			printf("\n%sError in forking read child!",KRED );
+			return ;
+		}
+
+		if(p2 == 0)
+		{
+			//Close the write end
+			close(pipe_file_desc[1]);
+			dup2(pipe_file_desc[0],STDIN_FILENO);
+			close(pipe_file_desc[0]);
+
+			if(execvp(pipecommands[0],pipecommands)<0)
+			{
+				printf("\n%sError in executing command 2!",KRED );
+				exit(0);	
+			}
+
+		}
+
+		else
+		{
+			wait(NULL);
+			wait(NULL);
+		}
+	}
+	
+}
+
+//Split input into the two commands of the pipe
+int parsePipedCommands(char *str, char ** strPipeCommands)
+{
+	int i=0;
+	//strsep splits the string whereever there's a |
+	for(;i<2;i++)
+	{
+		strPipeCommands[i] = strsep(str,"|");
+	}
+
+	//No piped command found if second argument is empty
+	if(strPipeCommands[1]==NULL)
+		return 0;
+
+	else
+		return 1;
+}
+
+
+//splits the input command into separate words 
+void findCommands(char *str, char **parsedCommands)
+{
+	int i=0;
+	for(;i<MAXCOMM;i++)
+	{
+		parsedCommands[i] = strsep(str," ");
+
+		if(parsedCommands[i]==NULL)
+			break;     //Since no more commands left
+
+		if(strlen(parsedCommands[i])==0)
+			i--;
+	}
+}
+
+int myCommands(char **commands)
+{
+	return 0;
+}
+
+int processCommands(char * input, char **strCommands ,char **strPipeCommands)
+{
+	char* commands[2];
+	//commands will hold the two parts incase of a pipe or just one otherwise
+
+	//checking for if pipeline is there 
+	int check_for_pipe = parsePipedCommands(input, commands);
+
+	//If pipeline exists break down the individual parts of the pipe into commands
+	if(check_for_pipe)
+	{
+		findCommands(commands[0], strCommands);
+		findCommands(commands[1], strPipeCommands);
+	}
+
+	else
+	{
+		findCommands(commands[0], strCommands);
+	}
+
+	// strCommands holds commands of the first part while strPipeCommands holds commands
+	// of the piped part
+
+	if (myCommands(strCommands))
+		return 0;
+
+	else 
+		return 1 + check_for_pipe; 
+
+
+}
+
+
 
 
 
